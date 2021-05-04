5b0783ad4e9f0efd33f2fdca7e2dc2d6baee813c
ImZeus0 <gyber25@gmail.com>
2021-05-04 17:53:52 +0300

    w

--- check_port.py
+++ check_port.py
@@ -1,7 +1,15 @@
 import subprocess
-data = subprocess.check_output(['iptables','-t','nat','-L','--line-numbers','-n'])
-data = data.decode('utf-8')
-lines_data = data.split('\n')
-for line in lines_data:
-    if 'dpt' in line:
-        print(line)
+
+
+
+
+def get_list_enable_proxy():
+    data = subprocess.check_output(['iptables', '-t', 'nat', '-L', '--line-numbers', '-n'])
+    data = data.decode('utf-8')
+    lines_data = data.split('\n')
+    for line in lines_data:
+        if 'dpt' in line:
+            print(line)
+
+if __name__ == "__main__":
+    get_list_enable_proxy()
